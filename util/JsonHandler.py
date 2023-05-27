f7a40b9ba4fc1c7d48c37768f15178ad40ec19ad
Cyn <70904206+CooldudePUGS@users.noreply.github.com>
2023-05-26 21:00:09 -0500

    Update JsonHandler.py

--- util/JsonHandler.py
+++ util/JsonHandler.py
@@ -20,4 +20,11 @@ def load_users():
 def save_users(data):
     with open('noreplyusers.json', 'w') as f:
         json.dump(data, f, indent=4)
-    
+def load_channels():
+    with open("allowedchannels.json", 'r') as f:
+        channels = json.load(f)
+    return channels
+
+def save_channels(data):
+    with open('allowedchannels.json', 'w') as f:
+        json.dump(data, f, indent=4)
