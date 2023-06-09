e54367d39bdff48306a489c374996f6ef663db30
Cyn <70904206+CooldudePUGS@users.noreply.github.com>
2023-06-08 19:56:51 -0500

    Update AllowedChannels.py

--- cogs/AllowedChannels.py
+++ cogs/AllowedChannels.py
@@ -3,33 +3,28 @@ import os
 import util.JsonHandler
 from discord.ext import commands
 
-log_data = {}
 
 class AllowedChannels(commands.Cog):
     def __init__(self, bot :commands.Bot) -> None:
         self.bot = bot
-
     @commands.hybrid_command(description="Enables automatic replies for the current channel. Owner only.")
     async def allowchannel(self, ctx):
         if ctx.author.id == self.bot.owner_id:
             data = util.JsonHandler.load_channels()
-            log_data[str(ctx.channel.id)] = []
-            log_data[str(ctx.channel.id)] = True
-            util.JsonHandler.save_channels(log_data)
-            await ctx.send("Successfully enabled automatic replies in this channel!", ephemeral=True)
+            data[str(ctx.channel.id)] = Enabled
+            util.JsonHandler.save_channels(data)
+            await ctx.send("Successfully enabled automatic replies in this channel!")
 
     @commands.hybrid_command(description="Disables automatic replies for the current channel. Owner only.")
     async def blockchannel(self, ctx):
         if ctx.author.id == self.bot.owner_id:
             data = util.JsonHandler.load_channels()
+            
+            if str(ctx.channel.id) in data:
+                del data[str(ctx.channel.id)]
+                await ctx.send("Successfully disabled automatic replies in this channel!")
 
-            for key in data:
-                if str(ctx.channel.id) in data:
-                    del data[str(ctx.channel.id)]
-                    await ctx.send("Successfully disabled automatic replies in this channel!", ephemeral=True)
-                    break
-        
-            util.JsonHandler.save_channels(data)
+        util.JsonHandler.save_channels(data)
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(AllowedChannels(bot))
