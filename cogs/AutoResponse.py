47294449306f62c61d81a5eae7bfd5ffa3f0a62b
Cyn <70904206+CooldudePUGS@users.noreply.github.com>
2023-06-09 10:17:28 -0500

    Update mod installation response

--- cogs/AutoResponse.py
+++ cogs/AutoResponse.py
@@ -11,7 +11,8 @@ help = discord.Embed(description="I noticed that you may have asked for help. Pl
 # Embed for automatically replying to potential questions about installing mods for Northstar
 installmods = discord.Embed(description="I noticed that you may have asked for help installing mods. You can do this automatically or manually.", color=0x5D3FD3)
 installmods.add_field(name="Automatic mod installation", value="Simply use a mod manager and navigate to the mods browser to automatically find and install mods.")
-installmods.add_field(name="Manual mod installation", value="When downloading a `.zip` folder for a mod from Thunderstore, you first want to unzip/extract it. Then, go into the extracted folder, go into the `mods` folder, and there will be a folder with a name similar to the mod. Move that folder to `Titanfall2\R2Northstar\mods` and your mod will be installed.\n\nIf I'm being accidentally triggered or annoying, please ping @cooldudepugs#4318")
+installmods.add_field(name="Manual mod installation", value="See the image sent below for help installing mods manually. If it's hard to read, click into it, and hit `Open in browser`, then follow the image guide.")
+installmods.add_field(name="\u200b", value="If I'm being accidentally triggered or annoying, please ping @cooldudepugs#4318", inline=False)
 # Embed for automatically replying to mentions of "Couldn't find player account"
 playeraccount = discord.Embed(description="I noticed that you may have asked for help regarding the \"Couldn't find player account\" error. Please read the [wiki section for this issue](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#player-not-found-invalid-master-server-token) to solve the error. \n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs#4318", color=0x5D3FD3)
 
