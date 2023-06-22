7d8d2de0c711916be06688abb53f5605ce64324c
Cyn <70904206+CooldudePUGS@users.noreply.github.com>
2023-06-22 08:01:35 -0500

    Clean up AutoResponse

--- cogs/AutoResponse.py
+++ cogs/AutoResponse.py
@@ -6,23 +6,29 @@ from cogs.GlobalReplies import replycheck
 
 
 # Embed for automatically replying to potential questions about installing Northstar
-installing = discord.Embed(description="I noticed that you may have asked for help installing Northstar. Please read the [installation channel](https://discordapp.com/channels/920776187884732556/922662496588943430) for ways to install Northstar.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs#4318", color=0x5D3FD3)
+installing = discord.Embed(description="I noticed that you may have asked for help installing Northstar. Please read the [installation channel](https://discordapp.com/channels/920776187884732556/922662496588943430) for ways to install Northstar.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
+
 # Embed for automatically replying to potential questions about help for Northstar
-help = discord.Embed(description="I noticed that you may have asked for help. Please open a ticket in the [help channel](https://discordapp.com/channels/920776187884732556/922663326994018366/1101924175343517827).\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs#4318", color=0x5D3FD3)
+help = discord.Embed(description="I noticed that you may have asked for help. Please open a ticket in the [help channel](https://discordapp.com/channels/920776187884732556/922663326994018366/1101924175343517827).\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
+
 # Embed for automatically replying to potential questions about installing mods for Northstar
 installmods = discord.Embed(description="I noticed that you may have asked for help installing mods. You can do this automatically or manually.", color=0x5D3FD3)
 installmods.add_field(name="Automatic mod installation", value="Simply use a mod manager and navigate to the mods browser to automatically find and install mods.")
 installmods.add_field(name="Manual mod installation", value="See the image sent below for help installing mods manually. If it's hard to read, click into it, and hit `Open in browser`, then follow the image guide.")
-installmods.add_field(name="\u200b", value="If I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs#4318", inline=False)
+installmods.add_field(name="\u200b", value="If I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", inline=False)
+
 # Embed for automatically replying to mentions of "Couldn't find player account"
-playeraccount = discord.Embed(description="I noticed that you may have asked for help regarding the \"Couldn't find player account\" error. Please read the [wiki section for this issue](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#player-not-found-invalid-master-server-token) to solve the error. \n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs#4318", color=0x5D3FD3)
+playeraccount = discord.Embed(description="I noticed that you may have asked for help regarding the \"Couldn't find player account\" error. Please read the [wiki section for this issue](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#player-not-found-invalid-master-server-token) to solve the error. \n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
+
 # Embed for automatically replying to mentions of "What is Northstar?"
-northstarInfo = discord.Embed(title="I noticed you may have asked a question about what Northstar is.", description="Northstar is a mod loader for Titanfall 2 with a focus on community server hosting and support for modding to replace models, sounds, textures, or even add new gamemodes. To install, you can check and read the [installation channel](https://discordapp.com/channels/920776187884732556/922662496588943430).\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs#4318")
+northstarInfo = discord.Embed(title="I noticed you may have asked a question about what Northstar is.", description="Northstar is a mod loader for Titanfall 2 with a focus on community server hosting and support for modding to replace models, sounds, textures, or even add new gamemodes. To install, you can check and read the [installation channel](https://discordapp.com/channels/920776187884732556/922662496588943430).\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs")
+
 # Embed for automatically replying to potential questions of script compilation errors- either temporary entirely or temporary version for the ModSettings merge into Northstar 1.15.0
 compError = discord.Embed(title="I noticed you may have asked for help regarding \"Script Compilation Error\" issues.", description="This error generally means that something has gone wrong with one of your mods and either\n1. You\'re missing a dependency for a mod you\'ve installed (note: if you're using a mod manager, that should auto install them for you) or\n2. A mod you have is conflicting with another mod you have installed.", color=0x5D3FD3)
-compError.add_field(name="Recently, ModSettings has been merged into Northstar. This means it\'s a part of normal Northstar, and having the mod version of it from now on will cause issues.", value="This will probably cause a lot of this specific issue for the next little while, so make sure to remove it if you haven\'t already\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs#4318")
+compError.add_field(name="Recently, ModSettings has been merged into Northstar. This means it\'s a part of normal Northstar, and having the mod version of it from now on will cause issues.", value="This will probably cause a lot of this specific issue for the next little while, so make sure to remove it if you haven\'t already\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs#")
+
 # Embed for automatically replying to potential mentions of "Authentication Failed", meant to be enabled when the Master Server Northstar is run on is down
-msdownembed = discord.Embed(title="I noticed you may have mentioned the error \"Authentication Failed\".", description="Currently, the Master server Northstar operates on is down. This means that currently you can't connect and aren't alone in having the error. Please wait for the master server to come back up and continue to check [the annoucements channel](https://discord.com/channels/920776187884732556/920780605132800080) for more updates.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs#4318", color=0x5D3FD3)
+msdownembed = discord.Embed(title="I noticed you may have mentioned the error \"Authentication Failed\".", description="Currently, the master server Northstar's server browser operates on is down. This means that currently you can't connect and aren't alone in having the error. Please wait for the master server to come back up and continue to check [the annoucements channel](https://discord.com/channels/920776187884732556/920780605132800080) for more updates.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
 
 responses = {  
     "install":         ["installing northstar", "install northstar", "get northstar", "download northstar", "downloading northstar"],
@@ -31,12 +37,12 @@ responses = {
     "playeraccount":   ["couldnt find player account", "couldn't find player account", "player account not found"],
     "whatisNorthstar": ["what is northstar", "what's northstar"],
     "ScriptCompError": ["script compilation error", "compilation error", "compile error"],
-    "msdown":          ["authentication failed", "cant join"]
+    "msdown":          ["authentication failed", "cant join", "can't join"]
 }
 
 config = util.JsonHandler.load_json("config.json")
 
-class SimpleView(discord.ui.View):
+class ToggleRepliesButton(discord.ui.View):
     @discord.ui.button(label="Toggle automatic bot replies", style=discord.ButtonStyle.success)
     async def disable(self, interaction: discord.Interaction, button: discord.ui.Button):
         data = util.JsonHandler.load_users()
@@ -63,16 +69,6 @@ class AutoResponse(commands.Cog):
         self.last_time = datetime.datetime.utcfromtimestamp(0)
         self.last_channel = 0
 
-    @commands.hybrid_command(description="Enables the message for master server being down. Allowed users only.")
-    async def msdownmessageon(self, ctx):
-        allowed_users = util.JsonHandler.load_allowed_users()
-
-        if str(ctx.author.id) in allowed_users:
-            global msdown
-            msdown = True
-            await ctx.send("Enabled the message for cases where Masterserver is down!")
-        else:
-            await ctx.send("You don't have permission to use this command!", ephemeral=True)
 
     @commands.hybrid_command(description="Display what phrases trigger Spectre's auto reply. Allowed users only.")
     async def autoreplytriggers(self, ctx):
@@ -91,21 +87,32 @@ class AutoResponse(commands.Cog):
         triggers.add_field(name="", value="\u200b", inline=False)
         triggers.add_field(name="Script compilation errors (Note: This is mostly likely temporary, or at the very least will change soon especially as the Mod Settings mention will most likely only stay for a week or two)", value='"script compilation error", "compilation error", "compile error"\n', inline=False)
         triggers.add_field(name="", value="\u200b", inline=False)
-        triggers.add_field(name="Master Server down messages (Note: This auto reply must be enabled by using `/enablemsdownmessage` and should only be enabled when MS is actually down!)", value='"authentication failed", "cant join"\n', inline=False)
+        triggers.add_field(name="Master server down messages (Note: This auto reply must be enabled by using `/enablemsdownmessage` and should only be enabled when MS is actually down!)", value='"authentication failed", "cant join"\n', inline=False)
 
         if str(ctx.author.id) in allowed_users:
             await ctx.send(embed=triggers, ephemeral=True)
         else:
             await ctx.send("You don't have permission to use this command!", ephemeral=True)
+            
+    @commands.hybrid_command(description="Enables the message for the master server being down. Allowed users only.")
+    async def msdownmessageon(self, ctx):
+        allowed_users = util.JsonHandler.load_allowed_users()
 
-    @commands.hybrid_command(description="Disables the message for master server being down. Allowed users only.")
+        if str(ctx.author.id) in allowed_users:
+            global msdown
+            msdown = True
+            await ctx.send("Enabled the message for cases where master server is down!")
+        else:
+            await ctx.send("You don't have permission to use this command!", ephemeral=True)
+            
+    @commands.hybrid_command(description="Disables the message for the master server being down. Allowed users only.")
     async def msdownmessageoff(self, ctx):
         allowed_users = util.JsonHandler.load_allowed_users()
 
         if str(ctx.author.id) in allowed_users:
             global msdown
             msdown = False
-            await ctx.send("Disabled the message for cases where Masterserver is down!")
+            await ctx.send("Disabled the message for cases where the master server is down!")
         else:
             await ctx.send("You don't have permission to use this command", ephemeral=True)
     
@@ -114,7 +121,7 @@ class AutoResponse(commands.Cog):
         view = SimpleView()
         users = util.JsonHandler.load_users()
         neverusers = util.JsonHandler.load_neverusers()
-        channel = util.JsonHandler.load_channels()
+        enabledchannels = util.JsonHandler.load_channels()
         time_diff = (datetime.datetime.utcnow() - self.last_time).total_seconds()
 
 
@@ -130,10 +137,9 @@ class AutoResponse(commands.Cog):
                 if str(message.author.id) in neverusers:
                         return
                 
-                if str(message.channel.id) in channel:
+                if str(message.channel.id) in enabledchannels:
                         # Should stop all bot messages
                         if message.author.bot:
-                            # print(f"Stopped my stupid ass from making an infinite message loop :3")
                             return
                 
                         elif any(x in message.content.lower() for x in responses["whatisNorthstar"]):
@@ -166,10 +172,6 @@ class AutoResponse(commands.Cog):
                         elif any(x in message.content.lower() for x in responses["playeraccount"]):
                             await message.channel.send(reference=message, embed=playeraccount, view=view)
                             print(f"Couldn\'t find player account embed reply sent")
-                        
-                        elif "<@&1120176677033611334>" in message.content.lower():
-                            await message.delete()
-                            await message.channel.send("fuck")
                             
                 self.last_time = datetime.datetime.utcnow()
             self.last_channel = message.channel.id
