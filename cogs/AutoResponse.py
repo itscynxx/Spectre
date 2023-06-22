d900501fdf33190cf0344957419a34b077fad6b3
Cyn <70904206+CooldudePUGS@users.noreply.github.com>
2023-06-22 10:50:58 -0500

    gaming

--- cogs/AutoResponse.py
+++ cogs/AutoResponse.py
@@ -3,13 +3,25 @@ import discord
 from discord.ext import commands
 import util.JsonHandler
 from cogs.GlobalReplies import replycheck
+import re
 
 
 # Embed for automatically replying to potential questions about installing Northstar
 installing = discord.Embed(description="I noticed that you may have asked for help installing Northstar. Please read the [installation channel](https://discordapp.com/channels/920776187884732556/922662496588943430) for ways to install Northstar.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
 
+# Embed for mod manager responses
+managerhelp = discord.Embed(description="I noticed you may have asked for help with mod managers. Try reading the guides below if you haven't already.", color=0x5D3FD3)
+managerhelp.add_field(name="FlightCore", value="[FlightCore guide](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/northstar-installers/flightcore-guide)")
+managerhelp.add_field(name="VTOL", value="[VTOL guide](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/northstar-installers/vtol-guide)")
+managerhelp.add_field(name="Viper", value="[Viper guide](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/northstar-installers/viper-guide)")
+managerhelp.add_field(name="\u200b", value="If you're still having issues with your respective mod manager, please open a ticket in the [help channel](https://discordapp.com/channels/920776187884732556/922663326994018366/1101924175343517827).\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", inline=False)
+
 # Embed for automatically replying to potential questions about help for Northstar
 help = discord.Embed(description="I noticed that you may have asked for help. Please open a ticket in the [help channel](https://discordapp.com/channels/920776187884732556/922663326994018366/1101924175343517827).\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
+
+# Embed for automatically replying to potential questions about a controller not working
+controller = discord.Embed(description="I noticed you may have asked for help regarding a controller not working.", color=0x5D3FD3)
+controller.add_field(name="\u200b", value="Try following the [controller not working](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#controller) wiki section for multiple ways you can fix a controller not working on Northstar.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs")
 
 # Embed for automatically replying to potential questions about installing mods for Northstar
 installmods = discord.Embed(description="I noticed that you may have asked for help installing mods. You can do this automatically or manually.", color=0x5D3FD3)
@@ -18,7 +30,7 @@ installmods.add_field(name="Manual mod installation", value="See the image sent 
 installmods.add_field(name="\u200b", value="If I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", inline=False)
 
 # Embed for automatically replying to mentions of "Couldn't find player account"
-playeraccount = discord.Embed(description="I noticed that you may have asked for help regarding the \"Couldn't find player account\" error. Please read the [wiki section for this issue](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#player-not-found-invalid-master-server-token) to solve the error. \n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
+playeraccount = discord.Embed(description="I noticed that you may have asked for help regarding the \"Couldn't find player account\" error. Please read the [wiki section for this issue](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#player-not-found-invalid-master-server-token) to solve the error.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
 
 # Embed for automatically replying to mentions of "What is Northstar?"
 northstarInfo = discord.Embed(title="I noticed you may have asked a question about what Northstar is.", description="Northstar is a mod loader for Titanfall 2 with a focus on community server hosting and support for modding to replace models, sounds, textures, or even add new gamemodes. To install, you can check and read the [installation channel](https://discordapp.com/channels/920776187884732556/922662496588943430).\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs")
@@ -30,15 +42,6 @@ compError.add_field(name="Recently, ModSettings has been merged into Northstar. 
 # Embed for automatically replying to potential mentions of "Authentication Failed", meant to be enabled when the Master Server Northstar is run on is down
 msdownembed = discord.Embed(title="I noticed you may have mentioned the error \"Authentication Failed\".", description="Currently, the master server Northstar's server browser operates on is down. This means that currently you can't connect and aren't alone in having the error. Please wait for the master server to come back up and continue to check [the annoucements channel](https://discord.com/channels/920776187884732556/920780605132800080) for more updates.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
 
-responses = {  
-    "install":         ["installing northstar", "install northstar", "get northstar", "download northstar", "downloading northstar"],
-    "help":            ["how do i fix", "help with Northsrtar", "somebody help", "anybody help", "how fix", "how to fix", "vtol error", "viper error", "flightcore error"],
-    "installmods":     ["help installing mods", "help getting mods"],
-    "playeraccount":   ["couldnt find player account", "couldn't find player account", "player account not found"],
-    "whatisNorthstar": ["what is northstar", "what's northstar"],
-    "ScriptCompError": ["script compilation error", "compilation error", "compile error"],
-    "msdown":          ["authentication failed", "cant join", "can't join"]
-}
 
 config = util.JsonHandler.load_json("config.json")
 
@@ -69,30 +72,6 @@ class AutoResponse(commands.Cog):
         self.last_time = datetime.datetime.utcfromtimestamp(0)
         self.last_channel = 0
 
-
-    @commands.hybrid_command(description="Display what phrases trigger Spectre's auto reply. Allowed users only.")
-    async def autoreplytriggers(self, ctx):
-        allowed_users = util.JsonHandler.load_allowed_users()
-
-        triggers = discord.Embed(title="Phrases that trigger Spectre's auto reply:")
-        triggers.add_field(name="Installation help", value='"installing northstar", "install northstar", "get northstar", "download northstar", "downloading northstar"\n', inline=False)
-        triggers.add_field(name="", value="\u200b", inline=False)
-        triggers.add_field(name="General help", value='"how do i fix", "help with Northsrtar", "somebody help", "anybody help", "how fix", "how to fix", "vtol error", "viper error", "flightcore error"\n', inline=False)
-        triggers.add_field(name="", value="\u200b", inline=False)
-        triggers.add_field(name="Mod installing help", value='"help installing mods", "help getting mods"\n', inline=False)
-        triggers.add_field(name="", value="\u200b", inline=False)
-        triggers.add_field(name="Player account not found help", value='"couldnt find player account", "couldn\'t find player account", "player account not found"\n', inline=False)
-        triggers.add_field(name="", value="\u200b", inline=False)
-        triggers.add_field(name="What is Northstar messages", value='"what is northstar", "what\'s northstar"\n', inline=False)
-        triggers.add_field(name="", value="\u200b", inline=False)
-        triggers.add_field(name="Script compilation errors (Note: This is mostly likely temporary, or at the very least will change soon especially as the Mod Settings mention will most likely only stay for a week or two)", value='"script compilation error", "compilation error", "compile error"\n', inline=False)
-        triggers.add_field(name="", value="\u200b", inline=False)
-        triggers.add_field(name="Master server down messages (Note: This auto reply must be enabled by using `/enablemsdownmessage` and should only be enabled when MS is actually down!)", value='"authentication failed", "cant join"\n', inline=False)
-
-        if str(ctx.author.id) in allowed_users:
-            await ctx.send(embed=triggers, ephemeral=True)
-        else:
-            await ctx.send("You don't have permission to use this command!", ephemeral=True)
             
     @commands.hybrid_command(description="Enables the message for the master server being down. Allowed users only.")
     async def msdownmessageon(self, ctx):
@@ -141,38 +120,47 @@ class AutoResponse(commands.Cog):
                         # Should stop all bot messages
                         if message.author.bot:
                             return
-                
-                        elif any(x in message.content.lower() for x in responses["whatisNorthstar"]):
+                        
+                        elif re.search("what.*northstar", message.content.lower()):
                             await message.channel.send(reference=message, embed=northstarInfo, view=view)
                             print(f"Northstar info embed reply sent")
-
-                        elif any(x in message.content.lower() for x in responses["msdown"]):
+                            
+                        elif re.search("player.account", message.content.lower()):
+                            await message.channel.send(reference=message, embed=playeraccount, view=view)
+                            print(f"Couldn\'t find player account embed reply sent")
+                        
+                        elif re.search("controller not working", message.content.lower()):
+                            await message.channel.send(reference=message, embed=controller, view=view)
+                            print("Controller embed reply sent")
+                            
+                        elif re.search("authentication.failed", message.content.lower()) or re.search("cant.*join", message.content.lower()):
                             if EnabledCheck() == True:
                                 await message.channel.send(reference=message, embed=msdownembed, view=view)
                             else:
                                 return
 
-                        elif any(x in message.content.lower() for x in responses["install"]):
+                        elif re.search("install.northstar", message.content.lower()):
                             await message.channel.send(reference=message, embed=installing, view=view)
                             print(f"Installing Northstar embed reply sent")
 
-                        elif any(x in message.content.lower() for x in responses["ScriptCompError"]):
+                        elif re.search("script.compilation.error", message.content.lower()):
                             await message.channel.send(reference=message, embed=compError, view=view)
-                            print(f"Comp Error embed reply sent")
+                            print(f"Comp error embed reply sent")
 
-                        elif any(x in message.content.lower() for x in responses["help"]):
+                        elif re.search("flightcore|viper|vtol", message.content.lower()) and re.search("issue|problem", message.content.lower()):
+                            await message.channel.send(reference=message, embed=managerhelp, view=view)
+                            print(f"Mod manager help embed reply sent")
+
+                        elif re.search("anybody.help", message.content.lower()): 
                             await message.channel.send(reference=message, embed=help, view=view)
                             print(f"Northstar help embed reply sent")
                     
-                        elif any(x in message.content.lower() for x in responses["installmods"]):
+                        elif re.search("install.mods", message.content.lower()):
                             await message.channel.send(reference=message, embed=installmods, view=view)
                             await message.channel.send("https://cdn.discordapp.com/attachments/942391932137668618/1069362595192127578/instruction_bruh.png")
                             print(f"Northstar mods installing embed reply sent")
                             
-                        elif any(x in message.content.lower() for x in responses["playeraccount"]):
-                            await message.channel.send(reference=message, embed=playeraccount, view=view)
-                            print(f"Couldn\'t find player account embed reply sent")
-                            
+                        
                 self.last_time = datetime.datetime.utcnow()
             self.last_channel = message.channel.id
 
