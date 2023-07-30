2123992229e21ad974b01d32aaeaae5faf3a3932
cyrv6737 <cyrv6737@protonmail.com>
2023-07-30 03:07:33 -0400

    Stripped whitespace and fixed no-sale price

--- Spectre.py
+++ Spectre.py
@@ -223,11 +223,23 @@ async def price(ctx, region: typing.Optional[app_commands.Choice[str]]):
         standard_price = soup.find("div", class_="discount_original_price")
 
 
-        message = f"Titanfall 2 is **ON SALE for {str(sale_percent)}% OFF**!\nStandard Price: **{standard_price.text}**\nSale Price: **{final_price.text}**\n<https://store.steampowered.com/app/1237970/Titanfall_2/>"
+        message = f"Titanfall 2 is **ON SALE for {str(sale_percent)}% OFF**!\nStandard Price: **{standard_price.text.strip()}**\nSale Price: **{final_price.text.strip()}**\n<https://store.steampowered.com/app/1237970/Titanfall_2/>"
         await ctx.send(message)
         return
     else:
-        message = f"Titanfall 2 is **not** on sale.\nCurrent Price: **{standard_price}**\n<https://store.steampowered.com/app/1237970/Titanfall_2/>"
+        
+        base_url = "https://store.steampowered.com/app/1237970/Titanfall_2/?cc="
+
+        if region is None:
+            response = requests.get(base_url + "US")
+        else:
+            response = requests.get(base_url + region.value)
+
+        soup = BeautifulSoup(response.content, "lxml")
+
+        standard_price = soup.find("div", class_="game_purchase_price")
+        
+        message = f"Titanfall 2 is **not** on sale.\nCurrent Price: **{standard_price.text.strip()}**\n<https://store.steampowered.com/app/1237970/Titanfall_2/>"
         await ctx.send(message)
         return
 
