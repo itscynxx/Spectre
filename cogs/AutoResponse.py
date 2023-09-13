00e8a834d79dce160a2ccb09819f96a0364122a0
Cyn <70904206+itscynxx@users.noreply.github.com>
2023-09-13 16:07:38 -0500

    Make auto response cleaner

--- cogs/AutoResponse.py
+++ cogs/AutoResponse.py
@@ -6,38 +6,15 @@ from cogs.GlobalReplies import replycheck
 import re
 import asyncio
 
-# Embed for automatically replying to potential questions about installing Northstar
-installing = discord.Embed(description="I noticed that you may have asked for help installing Northstar. Please read the [installation channel](https://discordapp.com/channels/920776187884732556/922662496588943430) for ways to install Northstar.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
-
-# Embed for automatically replying to potential questions about help for Northstar
-help = discord.Embed(description="I noticed that you may have asked for help. Please open a ticket in the [help channel](https://discordapp.com/channels/920776187884732556/922663326994018366/1101924175343517827).\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
-
-# Embed for automatically replying to potential questions about a controller not working
-controller = discord.Embed(description="I noticed you may have asked for help regarding a controller not working.", color=0x5D3FD3)
-controller.add_field(name="\u200b", value="Try following the [controller not working](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#controller) wiki section for multiple ways you can fix a controller not working on Northstar.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs")
+responseEmbed = discord.Embed(title="Automatic bot response", description="", color=0x5D3FD3)
 
 # Embed for automatically replying to potential questions about installing mods for Northstar
 installmods = discord.Embed(description="I noticed that you may have asked for help installing mods. You can do this automatically or manually.", color=0x5D3FD3)
 installmods.add_field(name="Automatic mod installation", value="Simply use a mod manager and navigate to the mods browser to automatically find and install mods.")
 installmods.add_field(name="Manual mod installation", value="See the image sent below for help installing mods manually. If it's hard to read, click into it, and hit `Open in browser`, then follow the image guide.")
-installmods.add_field(name="\u200b", value="If I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", inline=False)
-
-# Embed for automatically replying to mentions of "Couldn't find player account"
-playeraccount = discord.Embed(description="I noticed that you may have asked for help regarding the \"Couldn't find player account\" error. Please read the [wiki section for this issue](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#playeraccount) to solve the error.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
-
-# Embed for automatically replying to mentions of "What is Northstar?"
-northstarInfo = discord.Embed(title="I noticed you may have asked a question about what Northstar is.", description="Northstar is a mod loader for Titanfall 2 with a focus on community server hosting and support for modding to replace models, sounds, textures, or even add new gamemodes. To install, you can check and read the [installation channel](https://discordapp.com/channels/920776187884732556/922662496588943430).\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs")
 
 # Embed for automatically replying to potential mentions of "Authentication Failed", meant to be enabled when the Master Server Northstar is run on is down
 msdownembed = discord.Embed(title="I noticed you may have mentioned the error \"Authentication Failed\".", description="Currently, the master server Northstar's server browser operates on is down. This means that currently you can't connect and aren't alone in having the error. Please wait for the master server to come back up and continue to check [the annoucements channel](https://discord.com/channels/920776187884732556/920780605132800080) for more updates.\n\nIf I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", color=0x5D3FD3)
-
-# Embed for letting users know the fastify error response doesn't matter if they aren't hosting a server
-fastifyError = discord.Embed(title='I noticed you may have mentioned the "NO GAMESERVER RESPONSE"/"got fastify error response" error message while asking for help.', description="This error only applies if you are trying to host a server, in which case your ports likely aren't forwarded properly. If you aren't trying to host a server, your actual error is something else happening. Please look for other mentions of issues, or send a log.", color=0x5D3FD3)
-fastifyError.add_field(name="\u200b", value="If I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", inline=False)
-
-# Embed for default EA stuff
-ea = discord.Embed(title="I noticed you may have asked for help regarding the \"Couldn't write log file!\" error.", description="If you have the game installed on EA, please follow the [wiki section](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#cannot-write-log-file-when-using-northstar-on-ea-app) for solving this issue.", color=0x5D3FD3)
-ea.add_field(name="\u200b", value="If I'm being accidentally triggered or annoying, please disable replies with the button below or ping @cooldudepugs", inline=False)
 
 config = util.JsonHandler.load_json("config.json")
         
@@ -94,16 +71,13 @@ class AutoResponse(commands.Cog):
                                 print("No matching keyword was found")     
 
                         elif re.search("player.*account", message.content.lower()):
-                            await message.channel.send(reference=message, embed=playeraccount)
-                            print(f"Couldn\'t find player account embed reply sent")
+                            responseEmbed.add_field(name="Couldn't find player account error", value="Please read the [wiki section for this issue](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#playeraccount) to solve the error.")
 
-                        elif re.search("Failed creating log file", message.content.lower()):
-                            await message.channel.send(reference=message, embed=ea)
-                            print("Default EA App directory embed reply sent")
-                        
-                        elif re.search("controller not working", message.content.lower()) or re.search("can.i.use.controller.*northstar", message.content.lower()):
-                            await message.channel.send(reference=message, embed=controller)
-                            print("Controller embed reply sent")
+                        elif re.search("Failed.creating.log.file", message.content.lower()):
+                            responseEmbed.add_field(name="Default EA path issues", value="If you have the game installed on EA, please follow the [wiki section](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#cannot-write-log-file-when-using-northstar-on-ea-app) for solving this issue.")
+
+                        elif re.search("controller.not.working", message.content.lower()) or re.search("can.i.use.controller.*northstar", message.content.lower()):
+                            responseEmbed.add_field(name="Controller not working", value="Try following the [controller not working](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#controller) wiki section for multiple ways you can fix a controller not working on Northstar.")
                             
                         elif re.search("authentication.*failed", message.content.lower()) or re.search("cant.*join", message.content.lower()):
                             if util.MasterStatus.IsMasterDown() == True:
@@ -112,13 +86,22 @@ class AutoResponse(commands.Cog):
                                 return
 
                         elif re.search("how|help", message.content.lower()) and re.search("install.northstar", message.content.lower()):
-                            await message.channel.send(reference=message, embed=installing)
-                            print(f"Installing Northstar embed reply sent")
+                            if re.search("uninstall", message.content.lower()):
+                                responseEmbed.add_field(name="Uninstalling Northstar", value="Note that if you're wanting to play vanilla, you can simply launch the vanilla game on Steam (remove the `-northstar` launch arg if you added it).\n\nIf you still want to delete Northstar however, deleting `NorthstarLauncher.exe`, `Northstar.dll`, and the `R2Northstar` folder from the `titanfall2` directory should be enough.\n\nIf you want to clear absolutely everything, you can also delete `r2ds.bat` and `LEGAL.txt` from the `titanfall2` directory, and delete the `bin` folder in the `titanfall2 directory`, then verify the game's files to restore the vanilla files found in the bin folder.")
+                            else:
+                                responseEmbed.add_field(name="Installing Northstar", value="Please read the [installation channel](https://discordapp.com/channels/920776187884732556/922662496588943430) for ways to install Northstar.")
                     
+                        # note: rewrite this later
                         elif re.search("help|how", message.content.lower()) and re.search("titanfall|northstar", message.content.lower()) and re.search("install.*mods", message.content.lower()):
                             await message.channel.send(reference=message, embed=installmods)
                             await message.channel.send("https://cdn.discordapp.com/attachments/942391932137668618/1069362595192127578/instruction_bruh.png")
                             print(f"Northstar mods installing embed reply sent")
+
+                        if len(responseEmbed.fields) > 0:
+                            await message.channel.typing()
+                            await asyncio.sleep(3)
+                            await message.channel.send(reference=message, embed=responseEmbed)
+                            responseEmbed.clear_fields()
 
                 if message.channel.id == 937922165163065384:
 
