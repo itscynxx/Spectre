3f2ddbe3815520fb8007d0314e1693ecc49cf783
Cyn <70904206+itscynxx@users.noreply.github.com>
2023-11-19 10:38:08 -0600

    Update ImageResponse
    This is part of a very big update of fixing the repo
    None of the updates I've made via vscode have updated on here for a bit, so I'm doing it manually now

--- cogs/ImageResponse.py
+++ cogs/ImageResponse.py
@@ -6,7 +6,29 @@ import util.JsonHandler
 import os
 import discord
 
-imageReadEmbed = discord.Embed(title="Automatic Image Reading:", description="", color=0x5D3FD3)
+vanillaPlus = discord.Embed(description="If you're using VanillaPlus, please make sure to read the instructions [on the mod page](https://northstar.thunderstore.io/package/NanohmProtogen/VanillaPlus/) (if you're updating from an older version, the steps have changed, so please read it again)\n\nIf you AREN'T using VanillaPlus, make sure you didn't disable any core mods. You can see if they're enabled or disabled in `R2Northstar/enabledmods.json`. Either change `false` to `true` on disabled core mods, or delete `enabledmods.json` and ALL mods will automatically enable", color=0x5D3FD3)
+vanillaPlus.add_field(name="", value="Please note that I'm a bot automatically reading your image. There is a chance this information is wrong, in which case please ping @Cyn")
+
+operationNotPermitted = discord.Embed(description="EA's default install directory has some issues associated with it, which can be solved by following the [wiki section about this error](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#cannot-write-log-file-when-using-northstar-on-ea-app)", color=0x5D3FD3)
+operationNotPermitted.add_field(name="", value="Please note that I'm a bot automatically reading your image. There is a chance this information is wrong, in which case please ping @Cyn")
+
+modFailedSanity = discord.Embed(description="The \"Mod failed sanity check\" error is specific to FlightCore, and means that the mod isn't properly formatted and FlightCore can't automatically install it. However, you can still follow the [manual mod install guide](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/manual-installation#installing-northstar-mods-manually) to install the mod you wanted.", color=0x5D3FD3)
+modFailedSanity.add_field(name="", value="Please note that I'm a bot automatically reading your image. There is a chance this information is wrong, in which case please ping @Cyn")
+
+logFile = discord.Embed(description="I noticed you encountered the \"Failed creating log file!\" error.\n\nPlease follow the [wiki section](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#cannot-write-log-file-when-using-northstar-on-ea-app) for solving this issue.", color=0x5D3FD3)
+logFile.add_field(name="", value="Please note that I'm a bot automatically reading your image. There is a chance this information is wrong, in which case please ping @Cyn")
+
+msvcp = discord.Embed(description="The \"MSVCP120.dll\" or \"MSVCR120.dll\" error comes up when you're missing a dependency Titanfall 2 uses to run. Follow the [wiki section for this issue](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#msvcr) to solve it.", color=0x5D3FD3)
+msvcp.add_field(name="", value="Please note that I'm a bot automatically reading your image. There is a chance this information is wrong, in which case please ping @Cyn")
+
+playeraccount = discord.Embed(description="Try following the guide on solving the \"Couldn't find player account\" and \"Invalid master server token\" errors [here](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#playeraccount)", color=0x5D3FD3)
+playeraccount.add_field(name="", value="Please note that I'm a bot automatically reading your image. There is a chance this information is wrong, in which case please ping @Cyn")
+
+originOffline = discord.Embed(description="Try following the guide on solving the \"Origin Offline\" and \"Origin logged out\" errors [here](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#origin-offline)", color=0x5D3FD3)
+originOffline.add_field(name="", value="Please note that I'm a bot automatically reading your image. There is a chance this information is wrong, in which case please ping @Cyn")
+
+scriptComp = discord.Embed(description="From this image alone, we can't see what's causing the issue. Please send a screenshot of the console (open it by hitting the `~` key), or send the newest log. You can find logs in `Titanfall2/R2Northstar/logs`, with the newest being on the bottom by default.", color=0x5D3FD3)
+scriptComp.add_field(name="", value="Please note that I'm a bot automatically reading your image. There is a chance this information is wrong, in which case please ping @Cyn")
 
 class imageStuff(commands.Cog):
     def __init__(self, bot :commands.Bot) -> None:
@@ -21,46 +43,41 @@ class imageStuff(commands.Cog):
         if str(message.author.id) in users:
             return
         
-        if str(message.channel.id) in channels:
+        if str(message.channel.id) in channels or str(message.channel.name).startswith("ticket"):
             if message.attachments:
 
-                await message.attachments[0].save("image.png")
+                if message.attachments[0].filename.endswith(".jpg") or message.attachments[0].filename.endswith(".png"):
+                    await message.attachments[0].save("image.png")
+
                 image = Image.open('image.png')
                 text = pytesseract.image_to_string(image)
+                print(text)
 
-                if re.search("windows.protected.your.pc", text.lower()):
-                    imageReadEmbed.add_field(name="", value="Mod managers sometimes get auto flagged by Windows Defender because they don't have a digital signature, which Windows automatically (falsely) assumes is dangerous. These files, however, are not dangerous. They're all open source (so you can see all the code that make them up) if you want to confirm for yourself.\n\nYou can simply press `More Info` and `Run Anyway` to use the application. Please only do this when I respond if you're getting this message specifically for a Northstar mod manager, and nothing else.")
+                if re.search("encountered.client.script.compilation.error", text.lower()) and re.search("error|help", message.content.lower()):
+                    await message.channel.send(embed=scriptComp, reference=message)
 
-                if re.search("items.nut", text.lower()) and re.search("INVALID_REF", text.lower()):
-                    imageReadEmbed.add_field(name="", value="If you're encountering the \"INVALID_REF\" issue, please double check if you've removed any mods that do things like add skins to the game. A big cause of this is removing MoreSkins while a skin is equipped on a gun, making the game try to load something that doesn't exist. If you aren't sure or this didn't work, you'll have to reset multiplayer data, which should fix the issue. You can do this by opening the console on the main menu by hitting `~`, typing `ns_resetpersistence` , and then hitting enter.")
+                elif re.search("invalid.or.expired.masterserver.token", text.lower()) or re.search("couldn.find.player.account", text.lower()):
+                    await message.channel.send(embed=playeraccount, reference=message)
 
-                if re.search("Encountered.CLIENT.script.compilation.error", text.lower()) and re.search("error|help", message.content.lower()):
-                    imageReadEmbed.add_field(name="", value="From this image alone, we can't see what's causing the issue. Please send a screenshot of the console (open it by hitting the `~` key), or send the newest log. You can find logs in `Titanfall2/R2Northstar/logs`, with the newest being on the bottom by default.")
+                elif re.search("origin.offline", text.lower()) or re.search("origin_logged_out", text.lower()):
+                    await message.channel.send(embed=originOffline, reference=message)
 
-                if re.search("Invalid.or.expired.masterserver.token", text.lower()) or re.search("Couldn't.find.player.account", text.lower()):
-                    imageReadEmbed.add_field(name="", value="Try following the guide on solving the \"Couldn't find player account\" and \"Invalid master server token\" errors [here](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#playeraccount)")
+                elif re.search("MSVCP120|MSVCR120", text.lower()):
+                    await message.channel.send(embed=msvcp, reference=message)
 
-                if re.search("MSVCP120.dll", text.lower()):
-                    imageReadEmbed.add_field(name="", value="The \"MSVCP120.dll\" error comes up when you're missing a dependency Titanfall 2 uses to run. Follow the [wiki section for this issue](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#msvcr) to solve it.")
+                elif re.search("failed.creating.log.file", text.lower()):
+                    await message.channel.send(embed=logFile, reference=message)
 
                 # FlightCore specific errors
-                if re.search("Mod.failed.sanity.check", text.lower()):
-                    imageReadEmbed.add_field(name="", value="The \"Mod failed sanity check\" error is specific to FlightCore, and means that the mod isn't properly formatted and FlightCore can't automatically install it. However, you can still follow the [manual mod install guide](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/manual-installation#installing-northstar-mods-manually) to install the mod you wanted.")
+                elif re.search("mod.failed.sanity.check", text.lower()):
+                    await message.channel.send(embed=modFailedSanity, reference=message)
 
                 # Viper specific errors
-                if re.search("Unknown.error*an.unknown.error.occurred", text.lower()):
-                    imageReadEmbed.add_field(name="", value="If you're using Viper, please click on the error message shown in this image and send a screenshot of the actual error message that pops up afterwards.")
+                elif re.search("operation.not.permitted", text.lower()) and re.search("ea.games", text.lower()):
+                    await message.channel.send(embed=operationNotPermitted, reference=message)
 
-                if re.search("operation.not.permitted", text.lower()) and re.search("EA.Games", text.lower()):
-                    imageReadEmbed.add_field(name="", value="EA's default install directory has some issues associated with it, which can be solved by following the [wiki section about this error](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#cannot-write-log-file-when-using-northstar-on-ea-app)")
-
-                if len(imageReadEmbed.fields) > 0:
-                    if len(imageReadEmbed) > 1:
-                        return
-                    else:
-                        imageReadEmbed.add_field(name="", value="\nPlease note that I'm a bot automatically reading your image. There is a chance this information is wrong, in which case please ping @Cyn")
-                        await message.channel.send(embed=imageReadEmbed, reference=message)
-                        imageReadEmbed.clear_fields()
+                elif re.search("compile.error.undefined.variable", text.lower()) and re.search("progression_getpreference", text.lower()):
+                    await message.channel.send(embed=vanillaPlus, reference=message)
 
                 os.remove("image.png")
                 # is this bad? probably
